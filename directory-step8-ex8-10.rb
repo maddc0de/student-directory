e82badd9f6a77b27ac113b2b9ded2a4a157b55a5
maddc0de <maddc0de@outlook.com>
2023-03-07 21:49:39 +0000

    use select method to print students grouped by the same cohort

---
+++ directory-step8-ex8-10.rb
@@ -0,0 +1,49 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp.to_s
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    puts "Please enter worst enemy"
+    worst_enemy = gets.chomp.to_s
+    worst_enemy = "The World" if worst_enemy.empty?
+    puts "Please enter cohort month"
+    month = gets.chomp.to_sym.capitalize
+    month = "March" if month.empty?
+    # add the student hash to the array
+    students << {name: name, enemy: worst_enemy, cohort: month}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp.to_s
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  puts "which month of cohorts would you like to see?"
+  user_input = gets.chomp.to_sym.capitalize
+  cohort_group = students.select do |student|
+    student[:cohort] == user_input
+  end
+  puts cohort_group
+
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
