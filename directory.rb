0518535dd5b91af3c4ee2a6e5ba383a72a2f7b64
maddc0de <maddc0de@outlook.com>
2023-03-08 15:12:05 +0000

    create method to save student's data to a csv file

--- directory.rb
+++ directory.rb
@@ -1,3 +1,15 @@
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    cvs_line = student_data.join(",")
+    file.puts cvs_line
+  end
+  file.close
+end
+
 @students = [] # an empty array accessible to all methods
 
 def input_students
@@ -25,6 +37,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -40,6 +53,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
